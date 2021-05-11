9a453a79cbe3599c4a600db7ac4f1785a1b7ea9b
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-05-11 08:08:34 +0300

    creating the project and made the routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Route exact path="/">
+        <Homepage></Homepage>
+      </Route>
+      <Route exact path="/signup">
+        <Signup></Signup>
+      </Route>
+      <Route exact path="/login">
+        <Login></Login>
+      </Route>
+    </HashRouter>
   );
 }
 
---
+++ src/pages/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Signup extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="p-signup">
+                Signup
+            </div>
+        )
+    }
+}
+
+export default Signup;
