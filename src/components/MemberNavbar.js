73770b92b2065633b1f89cb45afc660b2ac94726
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-05-12 14:40:40 +0300

    create members navbar component

---
+++ src/components/MemberNavbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+
+
+class MemberNavbar extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/#/">HOA Systems</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/#/member-dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="/#/tenants">Tenants</Nav.Link>
+                        <Nav.Link href="/#/messages">Messages</Nav.Link>
+                        <Nav.Link href="/#/issues">Issues</Nav.Link>
+                        <Nav.Link href="/#/voting">Voiting</Nav.Link>
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/#/logou">Logout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default MemberNavbar;
