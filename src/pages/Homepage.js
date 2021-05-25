5132268211535d9ab8054f65e590622fce6e9a68
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-05-25 10:01:18 +0300

    test refs for github pages

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,12 +8,12 @@ class Homepage extends React.Component {
     render(){
         return(
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/#/">HOA Systems</Navbar.Brand>
+                <Navbar.Brand href="#/">HOA Systems</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                    <Nav.Link href="/#/login">Login</Nav.Link>
-                    <Nav.Link href="/#/signup">Sign Up</Nav.Link>
+                    <Nav.Link href="#/login">Login</Nav.Link>
+                    <Nav.Link href="#/signup">Sign Up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
