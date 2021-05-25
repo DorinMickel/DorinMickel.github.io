14ac2d40076a67dfc37b457bd234a03e6e7a0636
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-05-25 10:03:27 +0300

    fix of refs

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -10,19 +10,19 @@ class MainNavbar extends React.Component {
     render(){
         return(
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#/">HOA Systems</Navbar.Brand>
+                <Navbar.Brand href="/#/">HOA Systems</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="/#/dashboard">Dashboard</Nav.Link>
                         {(this.props.activeUser.isCommitteeMember) ? 
-                        <Nav.Link href="#/tenants">Tenants</Nav.Link> : null}
-                        <Nav.Link href="#/messages">Messages</Nav.Link>
-                        <Nav.Link href="#/issues">Issues</Nav.Link>
-                        <Nav.Link href="#/voting">Voting</Nav.Link>
+                        <Nav.Link href="/#/tenants">Tenants</Nav.Link> : null}
+                        <Nav.Link href="/#/messages">Messages</Nav.Link>
+                        <Nav.Link href="/#/issues">Issues</Nav.Link>
+                        <Nav.Link href="/#/voting">Voting</Nav.Link>
                     </Nav>
                     <Nav className="ml-auto">
-                        <Nav.Link href="#/">Logout</Nav.Link>
+                        <Nav.Link href="/#/">Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
