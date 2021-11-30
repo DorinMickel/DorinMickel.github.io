605001499b5c35f3075be55d4b767f0ca91d6044
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-11-30 10:28:03 +0700

    add hashHistory to hashRouter

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { createHashHistory } from 'history'
 // import reportWebVitals from './reportWebVitals';
 
+const history = createHashHistory()
+
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter history={history}>
     <App />
   </HashRouter>,
   document.getElementById('root')
